08173ddb4d855d709015502ae58a925050d4a346
Paul Cuciureanu <paulcu@gmail.com>
2019-01-16 22:22:08 -0500

    chore: initial commit with commit linting

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict'
+
+/**
+ * RangeCollection class
+ */
+class RangeCollection {
+  /**
+   * Adds a range to the collection
+   * @param {Array<number>} range - Array of two integers that specify beginning and end of range.
+   */
+  add(range) {
+    // TODO: implement this
+  }
+
+  /**
+   * Removes a range from the collection
+   * @param {Array<number>} range - Array of two integers that specify beginning and end of range.
+   */
+  remove(range) {
+    // TODO: implement this
+  }
+
+  /**
+   * Prints out the list of ranges in the range collection
+   */
+  print() {
+    // TODO: implement this
+  }
+}
+
+module.exports = RangeCollection
