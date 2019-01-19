7bc74c1e2aa0941265d2cee183b75faf4b9b06dc
Paul Cuciureanu <paulcu@gmail.com>
2019-01-18 20:48:48 -0500

    feat(simple): add remove case, re: #11

--- src/SimpleRangeCollection/index.js
+++ src/SimpleRangeCollection/index.js
@@ -40,8 +40,42 @@ class SimpleRangeCollection extends RangeCollection {
    * Removes a range from the collection
    * @param {Array<number>} range - Array of two integers that specify beginning and end of range.
    */
-  remove (range) {
-    // TODO: implement this
+  remove ([start, end]) {
+    this.ranges = [...this.ranges, -start, end] // fliped signs!
+    this.ranges.sort((a, b) => {
+      if (Math.abs(a) === Math.abs(b)) {
+        return a - b // reorder [x, -x] to [-x, x]: negative value first cancels range
+      } else {
+        return Math.abs(a) - Math.abs(b)
+      }
+    })
+
+    let inRange = false
+    let idx
+
+    for (idx = 0; idx < this.ranges.length - 1; idx++) {
+      if (inRange && this.ranges[idx] >= 0 && this.ranges[idx + 1] < 0) {
+        // when signs flip sequentially,
+        // we can safely remove the inner range
+        // sinde ranges addition is transitive, i.e.:
+        // the order in which we add ranges doesn't matter
+        // note: subtracting ranges on the other hand are not transitive: the order matters
+        this.ranges.splice(idx, 2)
+        idx -= 2 // since we just removed two consecutive elements
+        inRange = false
+      }
+      if (!(idx % 2) && this.ranges[idx] < 0 && this.ranges[idx + 1] >= 0) {
+        // remove all flipped sign pairs
+        this.ranges.splice(idx, 2)
+        idx -= 2 // since we just removed two consecutive elements
+      }
+
+      if (this.ranges[idx] >= 0) {
+        inRange = true
+      } else {
+        inRange = false
+      }
+    }
   }
 
   /**
--- test/SimpleRangeCollection.spec.js
+++ test/SimpleRangeCollection.spec.js
@@ -43,7 +43,7 @@ test('add preflow', t => {
   t.deepEqual(rc.ranges, [0, -5])
 })
 
-test.only('add multiple overlaps', t => {
+test('add multiple overlaps', t => {
   const rc = new SimpleRangeCollection()
   rc.add([3, 5])
   rc.add([7, 8])
@@ -51,6 +51,67 @@ test.only('add multiple overlaps', t => {
   rc.add([1, 11])
   // ?rc.ranges
   t.deepEqual(rc.ranges, [1, -11], 'the largest overarching range did not cancel all inner ranges')
+})
+
+test('remove same', t => {
+  const rc = new SimpleRangeCollection()
+
+  rc.add([1, 5])
+  rc.remove([1, 5])
+
+  // ?rc.ranges
+  t.deepEqual(rc.ranges, [])
+})
+
+test('remove all', t => {
+  const rc = new SimpleRangeCollection()
+
+  rc.add([3, 4])
+  rc.remove([1, 5])
+
+  // ?rc.ranges
+  t.deepEqual(rc.ranges, [])
+})
+test('remove inside', t => {
+  const rc = new SimpleRangeCollection()
+  rc.add([1, 10])
+  rc.remove([2, 4])
+  // ?rc.ranges
+  t.deepEqual(rc.ranges, [1, -2, 4, -10])
+})
+
+test('remove overflow', t => {
+  const rc = new SimpleRangeCollection()
+  rc.add([1, 5])
+  rc.remove([3, 8])
+  // ?rc.ranges
+  t.deepEqual(rc.ranges, [1, -3])
+})
+
+test('remove preflow', t => {
+  const rc = new SimpleRangeCollection()
+  rc.add([2, 5])
+  rc.remove([1, 3])
+  // ?rc.ranges
+  t.deepEqual(rc.ranges, [3, -5])
+})
+
+test('remove preflow - zero case', t => {
+  const rc = new SimpleRangeCollection()
+  rc.add([2, 5])
+  rc.remove([0, 3])
+  // ?rc.ranges
+  t.deepEqual(rc.ranges, [3, -5])
+})
+
+test('remove multiple overlaps', t => {
+  const rc = new SimpleRangeCollection()
+  rc.add([3, 5])
+  rc.add([7, 8])
+  rc.add([9, 11])
+  rc.remove([4, 10])
+  // ?rc.ranges
+  t.deepEqual(rc.ranges, [3, -4, 10, -11], 'the largest overarching range did not cancel all inner ranges')
 })
 
 test.skip('complex sequence', t => {
