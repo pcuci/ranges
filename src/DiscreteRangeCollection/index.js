6240f7da017f5bd2909d978c54aaa676993eed72
Paul Cuciureanu <paulcu@gmail.com>
2019-01-24 21:16:41 -0500

    feat(discrete): add diffing ranges function, re: 14

--- src/DiscreteRangeCollection/index.js
+++ src/DiscreteRangeCollection/index.js
@@ -44,6 +44,118 @@ function * mergeIt (r1, r2) {
   }
 }
 
+function * diffIt (r1, r2) {
+  let c1 = 0
+  let c2 = 0
+
+  while (c1 < r1.length || c2 < r2.length) {
+    console.log(c1, c2)
+    console.log([r1, r2])
+    if (c1 % 2 && c1 < r1.length) { // half-open minuend
+      if (c2 < r2.length) {
+        if (c2 % 2) { // half-open subtrahend
+          console.log(c1, c2)
+          console.log([r1, r2])
+          if (r2[c2] < r1[c1]) {
+            yield r2[c2]
+            console.log(r2[c2])
+          }
+          c2++
+        } else { // half-closed subtrahend
+          console.log(c1, c2)
+          console.log([r1, r2])
+          if (r2[c2] < r1[c1]) {
+            yield r2[c2]
+            console.log(r2[c2])
+            c2++
+          } else {
+            yield r1[c1]
+            console.log(r1[c1], [c1, c2], [r1.length, r2.length])
+            c1++
+          }
+        }
+      } else {
+        c1++
+      }
+    } else { // half-closed mineuend
+      if (c2 < r2.length) {
+        if (c2 % 2) { // half-open subtrahend
+          console.log(c1, c2)
+          console.log([r1, r2])
+          if (r1[c1] < r2[c2]) {
+            yield r2[c2]
+            console.log(r2[c2])
+            c1++
+          } else {
+            c2++
+          }
+        } else { // half-closed subtrahend
+          console.log(c1, c2)
+          console.log([r1, r2])
+          if (r2[c2] < r1[c1]) {
+            c2++
+          } else {
+            yield r1[c1]
+            console.log(r1[c1])
+            c1++
+          }
+        }
+      } else {
+        c1++
+      }
+    }
+  }
+}
+
+function deZero (ranges) {
+  let idx
+  for (idx = 0; idx < ranges.length - 1; idx++) {
+    if (ranges[idx] === ranges[idx + 1]) {
+      ranges.splice(idx, 2)
+      idx -= 2
+    }
+  }
+  return ranges
+}
+
+function diff (r1, r2) {
+  const set = new Set([...deZero(r1), ...deZero(r2)])
+  const sorted = Array.from(set.values()).sort((a, b) => a - b)
+
+  const result = []
+  let c1 = 0
+  let c2 = 0
+  let subtracting = false
+  let adding = false
+
+  for (let n of sorted) {
+    if (n === r1[c1]) {
+      adding = c1 % 2 ? false : true
+      if (adding && !subtracting) {
+        result.push(n)
+      }
+      if (!adding && !subtracting) {
+        result.push(n)
+      }
+      c1++
+    }
+
+    if (n === r2[c2]) {
+      subtracting = c2 % 2 ? false : true
+      if (adding && subtracting) {
+        result.push(n)
+      }
+      if (adding && !subtracting) {
+        result.push(n)
+      }
+      c2++
+    }
+  }
+  return result
+}
+
 module.exports = {
-  mergeIt
+  mergeIt,
+  diffIt,
+  diff
 }
--- test/DiscreteRangeCollection/add.spec.js
+++ test/DiscreteRangeCollection/add.spec.js
@@ -1,12 +1,134 @@
 'use strict'
 
 const test = require('ava').default
-const { mergeIt } = require('../../src/DiscreteRangeCollection')
+const { mergeIt, diffIt, diff } = require('../../src/DiscreteRangeCollection')
 
-test('merge iterator', t => {
-  t.deepEqual(Array.from(mergeIt([1, 6, 7, 8], [2, 4])), [1, 6, 7, 8])
+// test('merge iterator', t => {
+//   t.deepEqual(Array.from(mergeIt([1, 6, 7, 8], [2, 4])), [1, 6, 7, 8])
+// })
+
+// test('merge iterator 2', t => {
+//   t.deepEqual(Array.from(mergeIt([1, 3, 7, 8], [2, 4])), [1, 4, 7, 8])
+// })
+
+// test('diff 1', t => {
+//   t.deepEqual(Array
+//     .from(diffIt([1, 4], [2, 6])), [1, 2])
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+test('diff 2', t => {
+  t.deepEqual(Array
+    .from(diff([1, 4], [3, 6])), [1, 3])
 })
 
-test('merge iterator 2', t => {
-  t.deepEqual(Array.from(mergeIt([1, 3, 7, 8], [2, 4])), [1, 4, 7, 8])
+test('diff 322', t => {
+  t.deepEqual(Array
+    .from(diff([3, 6], [1, 4])), [4, 6])
 })
+
+test('diff 3122', t => {
+  t.deepEqual(Array
+    .from(diff([3, 6], [1, 14])), [])
+})
+
+test('diff 32122', t => {
+  t.deepEqual(Array
+    .from(diff([3, 6], [1, 2])), [3, 6])
+})
+
+test('diff 321242', t => {
+  t.deepEqual(Array
+    .from(diff([3, 6], [10, 12])), [3, 6])
+})
+
+test('diff 3231242', t => {
+  t.deepEqual(Array
+    .from(diff([3, 6], [4, 5])), [3, 4, 5, 6])
+})
+
+test('diff doght', t => {
+  t.deepEqual(Array
+    .from(diff([6, 6], [2, 8])), [])
+})
+
+test('diff dot', t => {
+  t.deepEqual(Array
+    .from(diff([3, 6], [4, 4])), [3, 6])
+})
+
+test('diff dogt', t => {
+  t.deepEqual(Array
+    .from(diff([3, 6], [2, 3])), [3, 6])
+})
+
+test('diff 4dogt', t => {
+  t.deepEqual(Array
+    .from(diff([3, 6], [6, 9])), [3, 6])
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// test('diff 3', t => {
+//   t.deepEqual(Array
+//     .from(diffIt([1, 4], [2, 6])), [1, 2])
+// })
+
+// test('diff 4', t => {
+//   t.deepEqual(Array
+//     .from(diffIt([1, 4], [2, 6])), [1, 2])
+// })
+
+// test('diff 5', t => {
+//   t.deepEqual(Array
+//     .from(diffIt([1, 4], [2, 6])), [1, 2])
+// })
+
+// test('diff iterator', t => {
+//   t.deepEqual(Array.from(diffIt([1, 4, 5, 10], [2, 6, 7, 8, 9, 15])), [1, 2, 6, 7, 8, 9])
+// })
+
+// test('diff iterator 2', t => {
+//   t.deepEqual(Array.from(mergeIt([1, 3, 5, 8], [2, 6])), [1, 4, 7, 8])
+// })
