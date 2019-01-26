fb03693be91549d435e32edf0713fa133aed846d
Paul Cuciureanu <paulcu@gmail.com>
2019-01-26 18:00:11 -0500

    docs: autogenerate documentation from jsdoc, re: #16

---
+++ src/index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const DiscreteRangeCollection = require('./DiscreteRangeCollection')
+const SimpleRangeCollection = require('./SimpleRangeCollection')
+const RangeCollection = require('./RangeCollection')
+
+module.exports = {
+  DiscreteRangeCollection,
+  SimpleRangeCollection,
+  RangeCollection
+}
